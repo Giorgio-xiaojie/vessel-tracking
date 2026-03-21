cca4c9b9798573c8201f78aacfabb8b38cb70512
xiaojie <zhengshijie0618@gmail.com>
2026-03-21 13:47:44 -0500

    add project files

---
+++ app.py
@@ -0,0 +1,160 @@
+from flask import Flask
+import psutil
+import os
+import time
+import subprocess
+import re
+
+
+MAX_CPU_TEMP=70
+MAX_CPU_USAGE=80
+
+
+def signal_level(dbm):
+    if dbm >= -50:
+        return 5
+    elif dbm >= -60:
+        return 4
+    elif dbm >= -70:
+        return 3
+    elif dbm >= -80:
+        return 2
+    else:
+        return 1
+
+def get_ais_status():
+    # 检测你的AIS程序是否在运行
+    for p in psutil.process_iter(['name']):
+        if 'ais_script_name' in p.info['name']:
+            return "Running"
+    return "Stopped"
+
+start = time.time()
+
+app = Flask(__name__)
+
+@app.route("/")
+def home(): 
+    wifi = subprocess.getoutput("iwconfig wlan0 | grep 'Signal level'")
+    quality = int(re.search(r'Link Quality=(\d+)', wifi).group(1))
+    max_quality = int(re.search(r'Link Quality=\d+/(\d+)', wifi).group(1))
+    signal_dbm = signal_level(int(re.search(r'Signal level=(-?\d+)', wifi).group(1)))
+    signal_dbm = signal_dbm * "█" + (5 - signal_dbm) * "[]"
+    quality_percent = (quality * 100 // max_quality)
+    end = int(time.time())
+    tm = int((end-start))
+    m = round((tm)//(60),0)
+    h = round((m)//(60),0)
+    d = round((h)//(24),0)
+    cpu_usage = psutil.cpu_percent(interval=1)
+    flag=get_ais_status()
+
+    try:
+        with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
+            temp = int(f.read()) / 1000.0
+    except:
+        temp = "无法获取"
+    
+    temp_color= "blue" if temp<MAX_CPU_TEMP else "red"
+    cpu_color= "blue" if cpu_usage<MAX_CPU_USAGE else "red"
+    wifi_color= "green" if quality_percent>60 else "orange"
+   
+    return f"""
+    <div style="text-align:center;">
+        <h1 style="text-align:center; color:black; margin-top:90px">
+        Vessel-tracking
+        </h1>
+      
+        <h1 style="text-align:center;color:grey;">
+        ______________________________________________________________________________________________________________
+        </h1>
+    
+        <p style="text-align:center;color:grey;font-size:15px">
+        This is a Vessel-tracking ADS-B feeder. For more information visit <a href="https://www.bilibili.com" target="_blank">here</a>. Below you can view information to determine if your site is functioning properly.
+        </p>
+
+        <form>
+            <button title="1" style="padding:0px 100px;">1</button>
+            <button title="3" style="padding:0px 100px;">2</button>
+            <button title="5" style="padding:0px 100px;">3</button>
+            <button title="7" style="padding:0px 100px;">4</button>
+        </form>
+
+        
+        <p style="text-align:center;color:black;font-size:15px">
+        CPU Temperature:<span style="color:{temp_color}"> {temp}°C</span>
+        </p>
+
+        <p style="text-align:center;color:black;font-size:15px">
+        CPU Load:<span style="color:{cpu_color}"> {cpu_usage}%</span>       
+        </p>
+
+        <p style="text-align:center;color:black;font-size:15px">
+        System Uptime:  {d}days , {h}hours , {m}minutes
+        </p>
+    
+        <p style="text-align:center;color:black;font-size:15px">
+        WiFi:<span style="color:{wifi_color}"> {quality_percent}%</span>
+        </p>
+
+        <p style="text-align:center;color:black;font-size:15px">
+        Signal:<span style="color:black"> {signal_dbm} </span>
+        </p>
+
+        <p style="text-align:center;color:black;font-size:15px">
+        AIS:<span style="color:black"> {flag} </span>
+        </p>
+   
+        <a href="https://www.google.com" target="_blank" style="color:blue">go to google</a>
+
+        <form action="/page1">
+            <button type="submit";
+                    style="font-size:30px;
+                    padding:0px 10px;
+                    margin-top:50px;
+                    background-color:skyblue;
+                    color:white;
+                    border-radius:10px;
+                    ">
+            Go to vessel map
+            </button>
+        <form>
+    </div>
+    <meta http-equiv="refresh" content="5">
+    """
+
+@app.route("/page1")
+def page1():
+    return """
+    <div style="text-align:center;">
+    <a href="https://www.bilibili.com" target="_blank">
+        <button style="font-size:30px;padding:0px 0px;">
+        enter bilibili
+        </button>
+    </a>
+    </div>
+    """
+    
+if __name__=="__main__":
+    app.run(debug=True,host="0.0.0.0",port=8888)
+
+
+#       <p style="text-align:center;color:black;font-size:15px">
+#       PiAware Version:
+#       </p>
+#
+#       <p style="text-align:center;color:black;font-size:15px">
+#       Dump1090-fa Version:
+#       </p>
+#
+#       <p style="text-align:center;color:black;font-size:15px">
+#       Dump978-fa Version:
+#       </p>
+#memory = psutil.virtual_memory()
+#disk = psutil.disk_usage('/')
+#print(f"--- 树莓派状态 ---")
+#print(f"CPU 使用率: {cpu_usage}%")
+#print(f"CPU 温度: {temp}°C")
+#print(f"内存使用: {memory.percent}% (剩余 {memory.available // 1024 // 1024}MB)")
+#print(f"磁盘使用: {disk.percent}%")
+#time = psutil.boot_time()
